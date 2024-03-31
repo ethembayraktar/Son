da6170ccb3fd37ff8f45e5e1492f6b1ddf912a85
ethembayraktar <e.ugurbayraktar@windowslive.com>
2024-04-01 02:45:16 +0300

    Models eklendi

---
+++ Models/Greeting.cs
@@ -0,0 +1,7 @@
+﻿namespace Son.Models;
+
+public class Greeting
+{
+    public int Id { get; set; }
+    public string Message { get; set; }
+}
---
+++ Models/SonDbContext.cs
@@ -0,0 +1,26 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using Son.Models; // Modelinizin bulunduğu namespace'i burada kullanın
+
+namespace Son.Models
+{
+    public class SonDbContext : DbContext
+    {
+        // DbContext için constructor oluşturun
+        public SonDbContext(DbContextOptions<SonDbContext> options)
+            : base(options)
+        {
+        }
+
+        // 'Greetings' tablosunu temsil eden DbSet özelliğini ekleyin
+        public DbSet<Greeting> Greetings { get; set; }
+
+        // Gerekirse, burada veritabanı şemasını daha da detaylandırmak için 
+        // OnModelCreating metodunu override edebilirsiniz.
+        // Örnek:
+        // protected override void OnModelCreating(ModelBuilder modelBuilder)
+        // {
+        //     // Model yapılandırmaları
+        // }
+    }
+}
